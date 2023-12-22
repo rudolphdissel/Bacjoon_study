30bb5f7089ae5a1e66fb2064e26d914aaa0556cf
rudolphdissel <146186681+rudolphdissel@users.noreply.github.com>
2023-12-22 14:12:34 +0900

    [Bronze II] Title: 분해합, Time: 2080 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,18 @@
+N=int(input())
+ans=0
+#1부터 N까지 다 확인할거야
+for i in range(N):
+    m=str(i)
+    if ans!=0:
+        break
+    bubunsum=0
+    for j in range(len(m)):
+        bubunsum+=int(m[j])
+    bunsum=bubunsum+i
+    if bunsum==N:
+        ans=i
+
+if ans==0: 
+    print(0)
+else:
+    print(ans)
