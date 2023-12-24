a73d673bbf6f46deedec53092f2e13c8a71fd7ee
rudolphdissel <146186681+rudolphdissel@users.noreply.github.com>
2023-12-24 16:42:44 +0900

    [Bronze II] Title: 완전제곱수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.py
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -2,9 +2,9 @@ perfect=[]
 M=int(input())
 N=int(input())
 for i in range(M,N+1):
-    for j in range(1,i+1):
-        if j**2==i:
-            perfect.append(i)
+    ar=round(i**0.5,1)**2
+    if ar==i:
+        perfect.append(i)
 if perfect==[]:
     print(-1)
 else:
