19fe420291963084202fecaeaa1c5175c12a8a49
rudolphdissel <146186681+rudolphdissel@users.noreply.github.com>
2023-12-27 12:24:44 +0900

    [Silver IV] Title: 빙고, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,47 +1,54 @@
+#초기화
 all_list=[]
-ans_count=0
-bingo=0
-ans=0
-play=1
+all_ans_list=[]
+
+#검사 함수
+def check():
+    c=0
+    for x in range(5): #가로 빙고 검사.
+        list_hap=0
+        for y in range(5):
+            list_hap+=all_list[x][y] 
+        if list_hap==0:
+            c+=1
+    for x in range(5): #세로 빙고 검사
+        list_hap=0
+        for y in range(5):
+            list_hap+=all_list[y][x]
+        if list_hap==0:
+            c+=1
+    list_hap=0
+    for x in range(5): #오른쪽 대각선 빙고검사
+        list_hap+=all_list[x][x]
+    if list_hap==0:
+        c+=1
+    list_hap=0
+    for x in range(5):
+        list_hap+=all_list[x][4-x]
+    if list_hap==0:
+        c+=1
+    return c
+#체크 함수
+def ccount(n):
+    for k in range(5): #플레이어가 쓴 숫자 위치를 찾아서 0으로 바꾼다.
+        for l in range(5):
+            if n==all_list[k][l]:
+                all_list[k][l]=0
+
+
+#입력 받기
 for i in range(5):
     num_list=list(map(int,input().split()))
     all_list.append(num_list)
 
-for j in range(5):
-    check_list=list(map(int,input().split()))
-    bingo=0
-    for m in range(5):
-        bingo=0
-        ans_count+=1
-        for k in range(5): #플레이어가 쓴 숫자 위치를 찾아서 0으로 바꾼다.
-            for l in range(5):
-                if check_list[m]==all_list[k][l]:
-                    all_list[k][l]=0
-        for x in range(5): #0으로 다 바꿨다면, 검사시작. (0,0)(0,1),(0,2)(0,3)(0,4)더하기.그합이 0이라면, 1빙고
-            list_hap=0
-            for y in range(5):
-                list_hap+=all_list[x][y] 
-            if list_hap==0:
-                bingo+=1
-        for x in range(5):
-            list_hap=0
-            for y in range(5):
-                list_hap+=all_list[y][x]
-            if list_hap==0:
-                bingo+=1
-        list_hap=0
-        for x in range(5):
-            list_hap+=all_list[x][x]
-        if list_hap==0:
-            bingo+=1
-        list_hap=0
-        for x in range(5):
-            list_hap+=all_list[x][4-x]
-        if list_hap==0:
-            bingo+=1
-        if bingo>=3 and play==1:
-            ans=ans_count
-            play=0
-        else:
-            bingo=0
-print(ans)
+#정답판 받기.
+for i in range(5):
+    for j in list(map(int,input().split())):
+        all_ans_list.append(j)
+
+#검사시작
+for i in range(25):
+    ccount(all_ans_list[i]) #먼저 빙고 체크하기.
+    if check()>=3: #한 번 3빙고뜨면 이제 확인 안 할거다
+        print(i+1)
+        break
