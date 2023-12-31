2de17915aa856e60bf6a2f08ff4672d6967c8605
rudolphdissel <146186681+rudolphdissel@users.noreply.github.com>
2023-12-31 16:41:33 +0900

    [Silver III] Title: 순열 사이클, Time: 572 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,29 +1,21 @@
 #함수 정의
-def dfs(graph,v,visited):
-    visited[v]+=1
-    if graph[v]==v:
-        return 1
-    elif visited[v]<=1:
-        if visited[graph[v]]==0:
-            dfs(graph,graph[v],visited)
-            return 1
-    else: 
-        return 0
+def dfs(v):
+    visited[v]=True
+    next=graph[v]
+    if not visited[next]:
+        dfs(next)
 
     
 #입력받기
-T=int(input())
-for i in range(T):
-    a=0
+for _ in range(int(input())):
+    result=0
     N=int(input())
-    graph=list(map(int,input().split()))
-    graph1=[0]
-    #그래프 정돈
-    for j in graph:
-        graph1.append(j)
+    graph=[0]+list(map(int,input().split()))
     #방문기록 초기화
-    visited=[0]*(N+1)
+    visited=[False]*(N+1)
     #함수 실행,정답 출력
     for k in range(1,N+1):
-        a+=dfs(graph1,k,visited)
-    print(a)
+        if not visited[k]:
+            dfs(k)
+            result+=1
+    print(result)
