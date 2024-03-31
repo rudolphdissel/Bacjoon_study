60ec14b4b6b0c97e19648cc8f3d1d2815fba4966
rudolphdissel <146186681+rudolphdissel@users.noreply.github.com>
2024-03-31 10:45:56 +0900

    [Silver II] Title: 차이를 최대로, Time: 148 ms, Memory: 35556 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -2,13 +2,13 @@ from itertools import permutations, combinations
 n=int(input())
 a=list(map(int,input().split()))
 b=list(permutations(a))
-board=[]
+ans=0
 for i in b:
-    score=0
+    t=0
     for j in range(n-1):
-        t=i[j]-i[j+1]
-        if t<0:
-            t*=-1
-        score+=t
-    board.append(score)
-print(max(board))
+        t+=abs(i[j]-i[j+1])
+    ans=max(ans,t)
+print(ans)
+
+#1. abs라는 함수가 있다.
+#2. 나처럼 리스트에 담지말고, 그 때 그 때 max를 뽑아서 비교하면 메모리 절약이 가능하다.
