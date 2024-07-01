d15f30e70ebbebea1b7e43957bb7933081791e9e
rudolphdissel <146186681+rudolphdissel@users.noreply.github.com>
2024-07-01 21:04:53 +0900

    [Silver II] Title: 예산, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,22 +1,23 @@
-import sys
-input = sys.stdin.readline
+N=int(input())
+reque= list(map(int,input().split()))
+resource= int(input())
 
-N = int(input())
-cities = list(map(int, input().split()))
-budgets = int(input()) # 예산
-start, end = 0, max(cities) # 시작 점, 끝 점
+reque.sort()
 
-# 이분 탐색
-while start <= end:
-    mid = (start+end) // 2
-    total = 0 # 총 지출 양
-    for i in cities:
-        if i > mid:
-            total += mid
-        else:
-            total += i
-    if total <= budgets: # 지출 양이 예산 보다 작으면
-        start = mid + 1
-    else: # 지출 양이 예산 보다 크면
-        end = mid - 1
+start=0
+end=reque[-1]
+answer=0
+while start<=end:
+    mid=(start+end)//2
+    total=0
+    for i in reque:
+        if i>mid:
+            total+=mid
+        elif i<=mid:
+            total+=i
+    if total>resource:
+        end=mid-1
+    elif total<=resource:
+        start=mid+1
 print(end)
+    
